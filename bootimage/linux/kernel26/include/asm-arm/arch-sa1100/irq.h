5a37937a43449a9058e18b001031bd6a24d5cc9e
fare <fare>
2004-06-30 18:39:12 +0000

    Removing obsolete file.

--- bootimage/linux/kernel26/include/asm-arm/arch-sa1100/irq.h
+++
@@ -1,7 +0,0 @@
-/* Jornada820 version based on ???
- * $Id: irq.h,v 1.2 2004/06/30 14:04:12 fare Exp $
- * Used to exist in 2.4. Now gone.
- * Any program that uses fixup_irq or do_IRQ should be updated.
- * See Documentation/arm/Interrupts
- */
-/* #error This file is obsolete and will be removed soon. */
