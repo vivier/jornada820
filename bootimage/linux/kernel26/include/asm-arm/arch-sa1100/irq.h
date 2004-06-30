9fd50492da27dfc1d2fb74f56dea11b9268076bb
fare <fare>
2004-06-30 14:04:12 +0000

    This file is obsolete.

--- bootimage/linux/kernel26/include/asm-arm/arch-sa1100/irq.h
+++ bootimage/linux/kernel26/include/asm-arm/arch-sa1100/irq.h
@@ -1,16 +1,7 @@
-/*
- * linux/include/asm-arm/arch-sa1100/irq.h
- * 
- * Author: Nicolas Pitre
- */
 /* Jornada820 version based on ???
- * $Id: irq.h,v 1.1 2004/06/24 19:57:38 fare Exp $
+ * $Id: irq.h,v 1.2 2004/06/30 14:04:12 fare Exp $
+ * Used to exist in 2.4. Now gone.
+ * Any program that uses fixup_irq or do_IRQ should be updated.
+ * See Documentation/arm/Interrupts
  */
-
-#define fixup_irq(x)	(x)
-
-/*
- * This prototype is required for cascading of multiplexed interrupts.
- * Since it doesn't exist elsewhere, we'll put it here for now.
- */
-extern void do_IRQ(int irq, struct pt_regs *regs);
+/* #error This file is obsolete and will be removed soon. */
