f836a88f5abb497515eb98d0b4eac750fbbbe98b
fare <fare>
2004-07-03 23:40:26 +0000

    Moving to the new driver model from upstream...

--- bootimage/linux/kernel26/drivers/pcmcia/sa1101.c
+++ bootimage/linux/kernel26/drivers/pcmcia/sa1101.c
@@ -8,7 +8,7 @@
  *
  * George Almasi (galmasi@optonline.net), 2004/1/24
  * Based on the sa1111_generic.c file.
- * $Id: sa1100_jornada820.c,v 1.4 2004/07/03 14:04:50 fare Exp $
+ * $Id: sa1101.c,v 1.1 2004/07/03 23:40:26 fare Exp $
  */
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -16,10 +16,19 @@
 #include <linux/device.h>
 #include <linux/init.h>
 
+#include <linux/interrupt.h>
+#include <asm/mach/irq.h>
+#include <asm/arch/irq.h>
+
 #include <asm/hardware.h>
 #include <asm/mach-types.h>
 #include <asm/irq.h>
 #include <asm/arch/hardware.h>
+
+#include <asm/hardware/sa1101.h>
+#include "soc_common.h"
+#include "sa11xx_core.h"
+#include "sa11xx_base.h"
 #include "sa1100_generic.h"
 
 
@@ -35,20 +44,33 @@ static struct pcmcia_irqs irqs[] = {
 	{ 1, IRQ_SA1101_S1_CDVALID,     "SA1101 CF card detect" },
 	{ 1, IRQ_SA1101_S1_BVD1_STSCHG, "SA1101 CF BVD1" },
 };
-static int sa1101_pcmcia_hw_init(struct sa1100_pcmcia_socket *skt)
+
+static irqreturn_t debug_irq_handler(unsigned int irq, void* dev_id, struct pt_regs *regs)
 {
+	printk("Got interrupt (%d). Ignoring.\n", irq); // DEBUG
+        return IRQ_HANDLED;
+}
+
+static int sa1101_pcmcia_hw_init(struct soc_pcmcia_socket *skt)
+{
+	int ret;
+	printk(__FUNCTION__ "... "); // DEBUG
 	if (skt->irq == NO_IRQ)
 		skt->irq = skt->nr ? IRQ_SA1101_S0_READY_NIREQ :
 			IRQ_SA1101_S1_READY_NIREQ;
-	return sa11xx_request_irqs(skt, irqs, ARRAY_SIZE(irqs));
+//	request_irq(skt->irq, debug_irq_handler, SA_INTERRUPT, "SA1101 NIREQ", NULL);
+//	set_irq_type(skt->irq, IRQT_BOTHEDGE);
+	ret = soc_pcmcia_request_irqs(skt, irqs, ARRAY_SIZE(irqs));
+	printk("%d\n", ret); // DEBUG
+	return ret;
 }
 
-static void sa1101_pcmcia_hw_shutdown(struct sa1100_pcmcia_socket *skt)
+static void sa1101_pcmcia_hw_shutdown(struct soc_pcmcia_socket *skt)
 {
-	sa11xx_free_irqs(skt, irqs, ARRAY_SIZE(irqs));
+	soc_pcmcia_free_irqs(skt, irqs, ARRAY_SIZE(irqs));
 }
 
-static void sa1101_pcmcia_socket_state(struct sa1100_pcmcia_socket *skt, struct pcmcia_state *state)
+static void sa1101_pcmcia_socket_state(struct soc_pcmcia_socket *skt, struct pcmcia_state *state)
 {
 	unsigned long status = PCSR;
 
@@ -60,25 +82,25 @@ static void sa1101_pcmcia_socket_state(struct sa1100_pcmcia_socket *skt, struct 
 	case 0:
 		state->detect = (status & PCSR_S0_detected)     ? 0 : 1;
 		state->ready  = (status & PCSR_S0_ready)        ? 1 : 0;
-		state->vs_3v  = (status & PCSR_S0_VS1)          ? 0 : 1;
-		state->vs_Xv  = (status & PCSR_S0_VS2)          ? 0 : 1;
 		state->bvd1   = (status & PCSR_S0_BVD1_nSTSCHG) ? 1 : 0;
 		state->bvd2   = (status & PCSR_S0_BVD2_nSPKR)   ? 1 : 0;
 		state->wrprot = (status & PCSR_S0_WP)           ? 1 : 0;
+		state->vs_3v  = (status & PCSR_S0_VS1)          ? 0 : 1;
+		state->vs_Xv  = (status & PCSR_S0_VS2)          ? 0 : 1;
 		break;
 	case 1:
 		state->detect = (status & PCSR_S1_detected)     ? 0 : 1;
 		state->ready  = (status & PCSR_S1_ready)        ? 1 : 0;
-		state->vs_3v  = (status & PCSR_S1_VS1)          ? 0 : 1;
-		state->vs_Xv  = (status & PCSR_S1_VS2)          ? 0 : 1;
 		state->bvd1   = (status & PCSR_S1_BVD1_nSTSCHG) ? 1 : 0;
 		state->bvd2   = (status & PCSR_S1_BVD2_nSPKR)   ? 1 : 0;
 		state->wrprot = (status & PCSR_S1_WP)           ? 1 : 0;
+		state->vs_3v  = (status & PCSR_S1_VS1)          ? 0 : 1;
+		state->vs_Xv  = (status & PCSR_S1_VS2)          ? 0 : 1;
 		break;
 	}
 }
 
-static int sa1101_pcmcia_configure_socket(struct sa1100_pcmcia_socket *skt, const socket_state_t *state)
+static int sa1101_pcmcia_configure_socket(struct soc_pcmcia_socket *skt, const socket_state_t *state)
 {
 	unsigned int rst, flt, vcc0, vcc1, vpp0, vpp1, mask0, mask1; // irq
   unsigned long flags;
@@ -161,14 +183,14 @@ static int sa1101_pcmcia_configure_socket(struct sa1100_pcmcia_socket *skt, cons
   return 0;
 }
 
-void sa1101_pcmcia_socket_init(struct sa1100_pcmcia_socket *skt)
+void sa1101_pcmcia_socket_init(struct soc_pcmcia_socket *skt)
 {
-	sa11xx_enable_irqs(skt, irqs, ARRAY_SIZE(irqs));
+	soc_pcmcia_enable_irqs(skt, irqs, ARRAY_SIZE(irqs));
 }
 
-void sa1101_pcmcia_socket_suspend(struct sa1100_pcmcia_socket *skt)
+void sa1101_pcmcia_socket_suspend(struct soc_pcmcia_socket *skt)
 {
-	sa11xx_disable_irqs(skt, irqs, ARRAY_SIZE(irqs));
+	soc_pcmcia_disable_irqs(skt, irqs, ARRAY_SIZE(irqs));
 }
 
 struct pcmcia_low_level sa1101_pcmcia_ops = {
@@ -181,7 +203,49 @@ struct pcmcia_low_level sa1101_pcmcia_ops = {
 	.socket_suspend		= sa1101_pcmcia_socket_suspend,
 };
 
-int __init pcmcia_sa1101_init(struct device *dev)
+static int pcmcia_probe(struct sa1101_dev *dev)
 {
-	return sa11xx_drv_pcmcia_probe(dev, &sa1101_pcmcia_ops, 0, 2);
+	return sa11xx_drv_pcmcia_probe(&dev->dev, &sa1101_pcmcia_ops, 0, 2);
 }
+static int __devexit pcmcia_remove(struct sa1101_dev *dev)
+{
+	soc_common_drv_pcmcia_remove(&dev->dev);
+	return 0;
+}
+static int pcmcia_suspend(struct sa1101_dev *dev, u32 state)
+{
+	return pcmcia_socket_dev_suspend(&dev->dev, state);
+}
+
+static int pcmcia_resume(struct sa1101_dev *dev)
+{
+	return pcmcia_socket_dev_resume(&dev->dev);
+}
+
+static struct sa1101_driver pcmcia_driver = {
+	.drv = {
+		.name	= "sa1101-pcmcia",
+	},
+	.devid		= SA1101_DEVID_PCMCIA,
+	.probe		= pcmcia_probe,
+	.remove		= __devexit_p(pcmcia_remove),
+	.suspend	= pcmcia_suspend,
+	.resume		= pcmcia_resume,
+};
+
+static int __init sa1101_drv_pcmcia_init(void)
+{
+	printk("init sa1101 pcmcia driver...\n"); // DEBUG
+	return sa1101_driver_register(&pcmcia_driver);
+}
+
+static void __exit sa1101_drv_pcmcia_exit(void)
+{
+	sa1101_driver_unregister(&pcmcia_driver);
+}
+
+module_init(sa1101_drv_pcmcia_init);
+module_exit(sa1101_drv_pcmcia_exit);
+
+MODULE_DESCRIPTION("SA1101 PCMCIA card socket driver");
+MODULE_LICENSE("GPL");
